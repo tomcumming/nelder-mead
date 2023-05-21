357a5576bbc491970005e5815c1699b178844cf0
Tom Cumming <mail@tomcumming.co.uk>
2023-05-21 19:51:28 +0100

    Create initial simplex from single point

--- src/Math/NelderMead/Simplex.hs
+++ src/Math/NelderMead/Simplex.hs
@@ -2,6 +2,7 @@ module Math.NelderMead.Simplex
   ( Simplex',
     Simplex,
     fromPoints,
+    fromPoint,
     pointsList,
     insert,
     takeWorst,
@@ -16,8 +17,9 @@ import Data.List.NonEmpty qualified as NE
 import Data.Map qualified as M
 import Data.Maybe (fromMaybe)
 import Data.Sized (Sized)
+import Data.Sized qualified as Sized
 import Data.Type.Natural (Succ)
-import Data.Vector (Vector)
+import Data.Vector (Vector, imap)
 import GHC.TypeLits (KnownNat)
 import Math.NelderMead.Point (Point (..))
 import Math.NelderMead.Point qualified as P
@@ -40,6 +42,21 @@ fromPoints errFn =
     . M.fromListWith (<>)
     . map (\p -> (errFn p, NE.singleton p))
     . toList
+
+-- | Create a Simplex from a point by extending by offset in each dimension
+fromPoint ::
+  (Ord e, Num a, KnownNat n, KnownNat (Succ n)) =>
+  (Point n a -> e) ->
+  Point n a ->
+  a ->
+  Simplex n e a
+fromPoint errFn (Point p) offset = fromPoints errFn (Sized.unsafeFromList' ps)
+  where
+    ps =
+      Point p : do
+        idx <- [0 .. Sized.length p - 1]
+        let p' = imap (\i x -> x + if i == idx then offset else 0) $ Sized.unsized p
+        [Point $ Sized.unsafeToSized' p']
 
 pointsList :: Simplex' (Succ d) n e a -> NE.NonEmpty (Point n a)
 pointsList (Simplex' s) =
--- test/Main.hs
+++ test/Main.hs
@@ -7,13 +7,9 @@ import Math.NelderMead.Simplex qualified as S
 
 main :: IO ()
 main = do
-  let errFn (Point p :: Point 2 Float) = sin (p Sized.!! 0) + cos (p Sized.!! 1)
   -- let errFn (Point p :: Point 2 Float) = (p Sized.!! 0) ** 2 + (p Sized.!! 1) ** 2
-
-  let ps :: [Point 2 Float] =
-        Point . Sized.unsafeFromList'
-          <$> [[10, 10], [10, 11], [11, 10]]
-          -- <$> [[0, 0], [0, -pi / 8], [-pi / 8, 0]]
-
-  let s :: S.Simplex 2 Float Float = S.fromPoints errFn $ Sized.unsafeFromList' ps
+  let errFn (Point p :: Point 2 Float) = sin (p Sized.!! 0) + cos (p Sized.!! 1)
+  -- let p :: Point 2 Float = Point $ Sized.unsafeFromList' [10, 10]
+  let p :: Point 2 Float = Point $ Sized.unsafeFromList' [0, 0]
+  let s = S.fromPoint errFn p 1
   mapM_ print $ take 30 $ nelderMead errFn s
